116181b8ec252f122c32c6ce0772b2e5afab6349
shagufta <anjumshagu@gmail.com>
2022-04-27 13:46:38 +0530

    updated

--- raahi/raahi/settings.py
+++ raahi/raahi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rd40mj15axf%1^ba78qu!!yz3#69uq$196#1b_njg2ft7@t654'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TRUE
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['raahi1299.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -119,10 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
