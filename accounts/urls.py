fd2079307f28917b44bef3a8991e6c95e7e3755e
shagufta <anjumshagu@gmail.com>
2022-05-05 18:41:19 +0530

    okkk

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,15 +24,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
     path('api/login/', LoginAPI.as_view(), name='Login'),
-    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('api/Logout/', knox_views.LogoutView.as_view(), name='Logout'),
+    path('api/Logoutall/', knox_views.LogoutAllView.as_view(), name='Logoutall'),
+    path('api/Change-password/', ChangePasswordView.as_view(), name='Change-password'),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 
    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-
-   
 ]
+
